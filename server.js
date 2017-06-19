bc9b17fbcd84c3fd53b97954cac5758440765bce
Rain Tomista <rftomista@gmail.com>
2017-06-19 13:59:06 +0800

    Update server.js

--- server.js
+++ server.js
@@ -9,5 +9,5 @@ app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req, res) {
-  res.sendFile('dist/index.html', { root : __dirname});
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
