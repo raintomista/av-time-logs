763668d9246784c643df3ddcff1aa856959b2d69
Rain Tomista <rftomista@gmail.com>
2017-07-04 03:05:57 +0800

    Add network checker to user dashboard

--- src/app/components/user/user-dashboard/user-dashboard.component.ts
+++ src/app/components/user/user-dashboard/user-dashboard.component.ts
@@ -1,5 +1,6 @@
+import { AuthenticationService } from './../../../services/authentication.service';
+import { NetworkService } from './../../../services/network.service';
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'user-dashboard',
   templateUrl: './user-dashboard.component.html',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private networkService: NetworkService, private authService: AuthenticationService) { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit(){
+    this.checkNetwork();
+  }
+
+
+  checkNetwork(){
+    this.networkService.getClientIP().subscribe(result => {
+      this.networkService.checkNetworkStatus(result.data.geo.ip).subscribe(network => {
+        if(network.data.status === 0 || network.data === null){
+          alert("Sorry but this network is blacklisted.")
+          this.authService.logout();
+        }
+      });
+    })
   }
 
 }
