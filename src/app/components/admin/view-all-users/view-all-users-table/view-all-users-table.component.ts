57fe39a5a974f47390f260e79613c06e8c25a8bd
Rain Tomista <rftomista@gmail.com>
2017-07-11 17:05:07 +0800

    Fix suspending user issues

--- src/app/components/admin/view-all-users/view-all-users-table/view-all-users-table.component.ts
+++ src/app/components/admin/view-all-users/view-all-users-table/view-all-users-table.component.ts
@@ -1,3 +1,4 @@
+import { TimelogService } from './../../../../services/timelog.service';
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../../../services/user.service';
@@ -6,14 +7,14 @@ import { UserService } from '../../../../services/user.service';
   selector: 'view-all-users-table',
   templateUrl: './view-all-users-table.component.html',
   styleUrls: ['./view-all-users-table.component.css'],
-  providers: [UserService]
+  providers: [UserService, TimelogService]
 
 })
 export class ViewAllUsersTableComponent implements OnInit {
   private users: Object[];
   private loading: Boolean;
 
-  constructor(private userService: UserService, private router: Router) { 
+  constructor(private userService: UserService, private router: Router, private timelogService: TimelogService) { 
     this.loading = true;
     this.userService.getUsers().subscribe(users =>{
       if(users.data.length > 0){
@@ -32,9 +33,10 @@ export class ViewAllUsersTableComponent implements OnInit {
   suspend(user){
     if (confirm(`Do you want to suspend this ${user.name}`) == true) {
       this.userService.suspendUser(user.username).subscribe(users =>{
-        alert(`Successfully suspended ${user.name}`);
-        user.status = 3;
-        
+        this.timelogService.timeOut(user.username).subscribe(response =>{
+          alert(`Successfully suspended ${user.name}`);
+          user.isSuspended = true;
+        });
       }); 
     } 
     else {
@@ -46,7 +48,7 @@ export class ViewAllUsersTableComponent implements OnInit {
     if (confirm(`Do you want to unsuspend this ${user.name}`) == true) {
       this.userService.unsuspendUser(user.username).subscribe(users =>{
         alert(`Successfully unsuspended ${user.name}`);
-        user.status = 0;
+        user.isSuspended = false;
       }); 
     } 
     else {
